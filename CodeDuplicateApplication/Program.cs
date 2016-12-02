065a58c106d3d19dcb947b76ac3b88730a0cd937
siashim <siashim@ymail.com>
2016-12-01 16:12:33 -0800

    Added program to GItHub
    The complete program is added to GitHub.

---
+++ CodeDuplicateApplication/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace CodeDuplicateApplication
+{
+    class Program
+    {
+        // Our directory path which will be created when the function is called
+        // this is used by the Simian
+        private static string directoryPath = 
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            + @"\Code_Duplicate_Checker_Program_Github_Code";
+
+        static void Main(string[] args)
+        {
+            string languageExtension = "";
+            bool languageCheck = true;
+            // Getting user input for two repositories
+            Console.WriteLine
+                ("Please type the address for a public GitHub repository.");
+            string url1 = Console.ReadLine();
+            Console.WriteLine("Please type the address for another"
+                + " public GitHub repository.");
+            string url2 = Console.ReadLine();
+            // Asks the user for language of the repositories
+            // This is used to give the right extension to Simian
+            while (languageCheck)
+            {
+                Console.WriteLine("Please type the language of the"
+                    + " repositories (Java, C#, C, C++, COBOL, Ruby," 
+                    + " JSP, ASP, HTML, XML, Visual Basic, Groovy).");
+                string language = Console.ReadLine();
+                switch (language)
+                {
+                    case "Java":
+                        languageExtension = ".java";
+                        languageCheck = false;
+                        break;
+                    case "C#":
+                        languageExtension = ".cs";
+                        languageCheck = false;
+                        break;
+                    case "C":
+                        languageExtension = ".c";
+                        languageCheck = false;
+                        break;
+                    case "C++":
+                        languageExtension = ".cpp";
+                        languageCheck = false;
+                        break;
+                    case "COBOL":
+                        languageExtension = ".cob";
+                        languageCheck = false;
+                        break;
+                    case "Ruby":
+                        languageExtension = ".rb";
+                        languageCheck = false;
+                        break;
+                    case "JSP":
+                        languageExtension = ".jsp";
+                        languageCheck = false;
+                        break;
+                    case "ASP":
+                        languageExtension = ".asp";
+                        languageCheck = false;
+                        break;
+                    case "HTML":
+                        languageExtension = ".htm";
+                        languageCheck = false;
+                        break;
+                    case "XML":
+                        languageExtension = ".xml";
+                        languageCheck = false;
+                        break;
+                    case "Visual Basic":
+                        languageExtension = ".vbp";
+                        languageCheck = false;
+                        break;
+                    case "Groovy":
+                        languageExtension = ".groovy";
+                        languageCheck = false;
+                        break;
+                    default:
+                        Console.WriteLine
+                            ("Please input one of the given options.");
+                        break;
+                }
+            }
+            // Calling all the functions for creating the folder
+            // and downloading and unpacking the GitHub repositories
+            RepositoryDownloader repDownloader = new RepositoryDownloader();
+            repDownloader.FolderCreator();
+            repDownloader.EmptyFolder();
+            repDownloader.ZipFileCreator();
+            repDownloader.Downlaod(url1, url2);
+            repDownloader.ZipFileExtractor();
+            repDownloader.ZipFileDeleter();
+            // Notifying user about the creation of new folder in My Documents
+            // Which is used to contain the unpacked GitHub repositories
+            Console.WriteLine("A new folder containing the GitHub"
+                 + " repositories has been created in My Documents.");
+            Console.WriteLine("Running Simian to check for duplicate code.");
+            System.Threading.Thread.Sleep(2500);
+    
+            // Openning cmd to run Simian to check for duplication
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            // Initializing the string that is used to 
+            // the directory that Simian is located
+            string changeToDirectory = @"cd C:\simian-2.4.0\bin\";
+            const string quote = "\"";
+            // Initializing the string that is used to give
+            // Simian the path to where the GitHub repositories are located
+            string runApplicationCommand = "simian-2.4.0.exe " + quote +
+                @"/" + directoryPath + @"**/*" + languageExtension + quote;
+
+            // Giving the string that changes the directory to Simian to cmd
+            cmd.StandardInput.WriteLine(changeToDirectory);
+            // Giving the string that runs Simian to cmd
+            cmd.StandardInput.WriteLine(runApplicationCommand);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
+        }
+    }
+}
+
---
+++ CodeDuplicateApplication/RepositoryDownloader.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO.Compression;
+using System.IO;
+
+namespace CodeDuplicateApplication
+{
+    class RepositoryDownloader
+    {
+        // Seetting the directory path to be in My Documents and naming the
+        // new directory
+        private static string directoryPath = 
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) 
+            + @"\Code_Duplicate_Checker_Program_Github_Code";
+        // Setting the paths for the zip files that is downloaded from 
+        // GitHub. The paths are based on the path of new directory
+        private static string ZipFilePath1= directoryPath
+            + @"\Repository 1.zip";
+        private static string ZipFilePath2 = directoryPath
+            + @"\Repository 2.zip";
+
+        // Cosntructor
+        public RepositoryDownloader()
+        {
+
+        }
+        // Creates the new folder if it already does not exists
+        public void FolderCreator ()
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+        // Empties the folder
+        public void EmptyFolder ()
+        {
+            if (Directory.Exists(directoryPath))
+            {
+                // Delete all files
+                foreach (var file in Directory.GetFiles
+                    (directoryPath))
+                {
+                    File.Delete(file);
+                }
+
+                // Delete all folders
+                foreach (var directory in Directory.GetDirectories
+                    (directoryPath))
+                {
+                    Directory.Delete(directory, true);
+                }
+            }
+        }
+        // Deletes the zip files that were downloaded from GitHub
+        public void ZipFileDeleter()
+        {
+            File.Delete(ZipFilePath1);
+            File.Delete(ZipFilePath2);
+        }
+        // Creates two empty zip files that are used to contain 
+        // the GitHub repositories 
+        public void ZipFileCreator()
+        {
+            using (FileStream zipToCreate =
+                new FileStream(ZipFilePath1, FileMode.CreateNew))
+            {
+                ZipArchive archive =
+                    new ZipArchive(zipToCreate, ZipArchiveMode.Create);
+            }
+            using (FileStream zipToCreate =
+                new FileStream(ZipFilePath2, FileMode.CreateNew))
+            {
+                ZipArchive archive =
+                    new ZipArchive(zipToCreate, ZipArchiveMode.Create);
+            }
+        }
+        // Extracts the zip files into two seperate directories within
+        // the directory that was created at the beginning
+        public void ZipFileExtractor()
+        {
+            ZipFile.ExtractToDirectory(ZipFilePath1, directoryPath
+                + @"\Repository 1");
+            ZipFile.ExtractToDirectory(ZipFilePath2, directoryPath 
+                + @"\Repository 2");
+        }
+        // Downloads the two GitHub repositories based on the user input
+        public void Downlaod(string gitHubUrl1, string gitHubUrl2)
+        {
+            // Token taken from GitHub
+            // This token only allows to use my own account not any account
+            // The token only allows the use of repositories and nothing else
+            var githubToken = "7192d8abfc7fe0436bc133c1de9293956a078525";
+            var url1 = gitHubUrl1 + @"/archive/master.zip";
+            var url2 = gitHubUrl2 + @"/archive/master.zip";
+            var path = ZipFilePath1;
+            // This downlaods the GitHub repositories based on first address
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                var credentials = string.Format
+                    (System.Globalization.CultureInfo.InvariantCulture,
+                    "{0}:", githubToken);
+                credentials = Convert.ToBase64String
+                    (System.Text.Encoding.ASCII.GetBytes(credentials));
+                client.DefaultRequestHeaders.Authorization = 
+                    new System.Net.Http.Headers.AuthenticationHeaderValue
+                    ("Basic", credentials);
+                var contents = client.GetByteArrayAsync(url1).Result;
+                System.IO.File.WriteAllBytes(path, contents);
+            }
+            // This downlaods the GitHub repositories based on second address
+            var secondPath = ZipFilePath2;
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                var credentials = string.Format
+                    (System.Globalization.CultureInfo.InvariantCulture,
+                    "{0}:", githubToken);
+                credentials = Convert.ToBase64String
+                    (System.Text.Encoding.ASCII.GetBytes(credentials));
+                client.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue
+                    ("Basic", credentials);
+                var contents = client.GetByteArrayAsync(url2).Result;
+                System.IO.File.WriteAllBytes(secondPath, contents);
+            }
+        }
+    }
+}
